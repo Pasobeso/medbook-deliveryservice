f256babc748824208efc77d181a6253a8e86c94c
Ittiwat Chuchoet <ciaabcdefg@gmail.com>
2025-10-19 19:30:07 +0700

    added migrations

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 use anyhow::Result;
 use axum::Router;
-use medbook_core::bootstrap::bootstrap;
+use diesel_migrations::{EmbeddedMigrations, embed_migrations};
+use medbook_core::{
+    bootstrap::{self, bootstrap},
+    config, db,
+};
 use medbook_deliveryservice::{consumers, routes};
+
+/// Migrations embedded into the binary which helps with streamlining image building process
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    bootstrap::init_tracing();
+    bootstrap::init_env();
+
     let app = Router::new()
         .merge(routes::deliveries::routes())
         .merge(routes::delivery_addresses::routes())
         .merge(routes::patients::delivery_addresses::routes());
 
+    tracing::info!("Running migrations...");
+    let config = config::load()?;
+    let migrations_count = db::run_migrations_blocking(MIGRATIONS, &config.database.url).await?;
+    tracing::info!("Run {} new migrations successfully", migrations_count);
+
+    tracing::info!("Bootstrapping...");
     bootstrap(
         "DeliveryService",
         app,
